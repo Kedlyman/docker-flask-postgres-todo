db87489cf64f40ec13baab3043a6f4c67c83ec60
Kedlyman <152236942+Kedlyman@users.noreply.github.com>
2025-04-27 10:47:46 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,5 @@
-import os
-
 class Config:
-    DB_HOST = os.getenv('DB_HOST', 'db')
-    DB_NAME = os.getenv('POSTGRES_DB', 'todo_db')
-    DB_USER = os.getenv('POSTGRES_USER', 'postgres')
-    DB_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'postgres')
+    DB_HOST = "db"
+    DB_NAME = "todo_db"
+    DB_USER = "postgres"
+    DB_PASSWORD = "postgres"
